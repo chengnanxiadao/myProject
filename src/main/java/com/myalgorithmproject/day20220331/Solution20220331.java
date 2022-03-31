8fd63865ad4a6c70c700185e9d74b2dcceb4be51
chengnanxiadao <1510002484@qq.com>
2022-03-31 23:26:45 +0800

    2022-03-31 easy + cv

---
+++ src/main/java/com/myalgorithmproject/day20220331/Solution20220331.java
@@ -0,0 +1,29 @@
+package com.myalgorithmproject.day20220331;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution20220331 {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividing(i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    public boolean isSelfDividing(int num) {
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+
+}
