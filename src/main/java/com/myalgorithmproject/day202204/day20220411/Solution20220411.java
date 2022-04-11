f2bc294d15aa6c159afd6af486648fbb062157f2
chengnanxiadao <1510002484@qq.com>
2022-04-11 20:57:13 +0800

    2022-04-11 medium + cv

---
+++ src/main/java/com/myalgorithmproject/day202204/day20220411/Solution20220411.java
@@ -0,0 +1,14 @@
+package com.myalgorithmproject.day202204.day20220411;
+
+class Solution20220411 {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 10;
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + (dp[i-1] - dp[i-2])*(10-(i-1));
+        }
+        return dp[n];
+    }
+}
