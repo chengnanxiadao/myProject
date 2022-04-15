9da2fa6093bd49713e7cb625d31ca59c4c49f131
chengnanxiadao <1510002484@qq.com>
2022-04-15 23:55:47 +0800

    2022-04-15 medium + cv

---
+++ src/main/java/com/myalgorithmproject/day202204/day20220415/Solution20220415.java
@@ -0,0 +1,7 @@
+package com.myalgorithmproject.day202204.day20220415;
+
+public class Solution20220415 {
+    public static void main(String[] args) {
+        NestedInteger s = new NestedInteger();
+    }
+}
