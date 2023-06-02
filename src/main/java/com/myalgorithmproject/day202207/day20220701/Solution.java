b64d3b91f602718ea869f1a574355bbec3b756ce
chengnanxiadao <1510002484@qq.com>
2023-06-03 02:45:45 +0800

    剑指005 单词长度的最大乘积

--- src/main/java/com/myalgorithmproject/day202207/day20220701/Solution.java
+++ src/main/java/com/myalgorithmproject/day202207/day20220701/Solution.java
@@ -580,3 +580,27 @@ class Solution48 {
         System.out.println(singleNumber);
     }
 }
+
+class Solution49 {
+    public int maxProduct(String[] words) {
+        // 看了题解，纯纯布隆过滤器的思路
+        int res = 0;
+        int[] nums = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                nums[i] |= (1 << words[i].charAt(j) - 'a');
+            }
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if((nums[i] & nums[j]) ==0){
+                    res = Math.max(res,words[i].length()*words[j].length());
+                }
+
+            }
+
+        }
+        return res;
+
+    }
+}
