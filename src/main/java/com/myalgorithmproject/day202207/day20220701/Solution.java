ccb91cb11fa9823471b2fe185ea3aa35ae184417
chengnanxiadao <1510002484@qq.com>
2023-06-05 00:08:03 +0800

    剑指006 两数之和

--- src/main/java/com/myalgorithmproject/day202207/day20220701/Solution.java
+++ src/main/java/com/myalgorithmproject/day202207/day20220701/Solution.java
@@ -604,3 +604,22 @@ class Solution49 {
 
     }
 }
+
+class Solution50 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left<right) {
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                return new int[]{left, right};
+            }
+        }
+        return null;
+
+
+    }
+}
